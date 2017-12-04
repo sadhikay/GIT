03db2dd364a95bf42d51ce74dd7ef73997b656b5
sadhika yezzu <sadhika.yezzu@hotmail.com>
2017-12-04 08:33:45 -0600

    Adding a .net application

---
+++ WebApplication1/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static string GetConnectionString(string key)
+        {
+            return ConfigurationManager.ConnectionStrings[key.ToLower()].ToString();
+        }
+
+        public ActionResult Index()
+        {
+            return View("About");
+        }
+
+        public ActionResult About()
+        {
+           return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public JsonResult GetData(string name)
+        {
+            string ConnectionString = GetConnectionString("myDB");
+
+            List<Data> list = new List<Data>();
+            SqlConnection conn;
+            SqlDataReader rdr;
+            using (conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Accounts$ where Name ='" + name+"'", conn);
+                cmd.CommandType = System.Data.CommandType.Text;
+                rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    list.Add(new Data()
+                    {
+                        Name = rdr["Name"].ToString(),
+                        Balance =Convert.ToDouble(rdr["Balance"]),
+                        Number =Convert.ToInt32(rdr["Balance"]),
+                    });
+            }
+            rdr.Close();
+
+
+        }
+
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/WebApplication1/Models/myModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+  public class Data
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public double Balance { get; set; }
+    }
+}
